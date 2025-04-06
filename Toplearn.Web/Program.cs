04b0db6a10b1ad8ccd63e7f66dea4fefb573eb23
Mehdi Mohseni <wuka.sec@gmail.com>
2025-04-06 15:27:18 +0330

    Add Template

--- Toplearn.Web/Program.cs
+++ Toplearn.Web/Program.cs
@@ -2,13 +2,30 @@ using Microsoft.EntityFrameworkCore;
 using Toplearn.DataLayer.Context;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddMvc();
+
+builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ToplearnDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
 app.UseStaticFiles();
-app.MapGet("/", () => "Hello World!");
+
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
