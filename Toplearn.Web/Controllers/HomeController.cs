cd27ff10f38e6ed0da383f9d5bf8d9ebc7eaa2ca
Mehdi Mohseni <wuka.sec@gmail.com>
2025-04-07 13:20:22 +0330

    Add basic Action() to Home controller for load pages

--- Toplearn.Web/Controllers/HomeController.cs
+++ Toplearn.Web/Controllers/HomeController.cs
@@ -8,4 +8,29 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    [Route("Features")]
+    public IActionResult Features()
+    {
+        return View();
+    }
+
+    [Route("Pricing")]
+    public IActionResult Pricing()
+    {
+        return View();
+    }
+
+    [Route("Blog")]
+    public IActionResult Blog()
+    {
+        return View();
+    }
+
+    [Route("Contact")]
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
 }
