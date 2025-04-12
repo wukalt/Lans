8f78d8cdaf5caa99ab77e06599bb8c95e30e53ec
Mehdi Mohseni <wuka.sec@gmail.com>
2025-04-12 21:16:57 +0330

    fix data anotations in models

--- Lans.DataLayer/Entities/User/Role.cs
+++ Lans.DataLayer/Entities/User/Role.cs
@@ -7,7 +7,7 @@ public class Role
     [Key]
     public int Id { get; set; }
 
-    [Display(Name = "")]
+    [Display(Name = "نقش")]
     [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
     [MaxLength(200, ErrorMessage = "{0} نمیتواند بیشتر از {1} باشد")]
     public string RoleTitle { get; set; } = null!;
--- Lans.DataLayer/Entities/User/User.cs
+++ Lans.DataLayer/Entities/User/User.cs
@@ -7,34 +7,34 @@ public class User
     [Key]
     public int UserId { get; set; }
 
-    [Display(Name = "")]
-    [Required(ErrorMessage = "")]
-    [MaxLength(200, ErrorMessage = "")]
+    [Display(Name = "نام")]
+    [Required(ErrorMessage = "نام اجباری میباشد")]
+    [MaxLength(200, ErrorMessage = "طول نام نمیتواند بیشتر از {0} باشد")]
     public string UserName { get; set; } = null!;
 
-    [Display(Name = "")]
-    [Required(ErrorMessage = "")]
-    [MaxLength(200, ErrorMessage = "")]
-    [EmailAddress(ErrorMessage = "")]
+    [Display(Name = "ایمیل")]
+    [Required(ErrorMessage = "ایمیل اجباری میباشد")]
+    [MaxLength(200, ErrorMessage = "طول ایمیل نمیتواند از {0} بیشتر باشد")]
+    [EmailAddress(ErrorMessage = "فرمت وارد شده درست نمیباشد")]
     public string Email { get; set; } = null!;
 
-    [Display(Name = "")]
-    [Required(ErrorMessage = "")]
-    [MaxLength(100, ErrorMessage = "")]
+    [Display(Name = "کلمه عبور")]
+    [Required(ErrorMessage = "کلمه عبور اجباری میباشد")]
+    [MaxLength(100, ErrorMessage = "طول کلمه عبور نمیتواند از {0} بیشتر باشد")]
     public string Password { get; set; } = null!;
 
-    [Display(Name = "")]
-    [MaxLength(50, ErrorMessage = "")]
+    [Display(Name = "کد فعالسازی")]
+    [MaxLength(50, ErrorMessage = "طول کد فعالسازی نمیتواند از {0} بیشتر باشد")]
     public string? ActiveCode { get; set; }
 
-    [Display(Name = "")]
+    [Display(Name = "وضعیت")]
     public bool IsActive { get; set; }
 
-    [Display(Name = "")]
-    [MaxLength(200, ErrorMessage = "")]
+    [Display(Name = "آواتار")]
+    [MaxLength(200, ErrorMessage = "طول آواتار نمیتواند از {0} بیشتر باشد")]
     public string? UserAvatar { get; set; }
 
-    [Display(Name = "")]
+    [Display(Name = "تاریخ ثبت نام")]
     public DateTime RegisterDate { get; set; }
 
     #region Relations
